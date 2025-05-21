cd86592bfaefaaefc98b9b50f49a11ea93c5b275
Marc Pete Casenas <dakillah@Siegharts-Mac.local>
2025-05-21 15:12:55 +0800

    additional changes to add documents to mongodb Atlas via JSON files.

--- Server/connectAtlas.js
+++ Server/connectAtlas.js
@@ -17,6 +17,16 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // Make the appropriate DB calls
+    const db = client.db("songlist");
+    const collection = db.collection("scores");
+
+    //Query Artist
+    const query = {title: "Angel"};
+    const queryResult = await collection.findOne(query);
+    console.log("Results: ", queryResult);
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
