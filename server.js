93d19649e2edc8eebdfef6a7384cc7decea975ba
Marc Pete Casenas <dakillah@Siegharts-Mac.local>
2025-06-08 00:20:50 +0800

    Fixing code error to handle index.html and index2.html

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/listSongs', async function (req, res)
     res.json(songsList);
 })
 
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./index.html"));
 });
 
