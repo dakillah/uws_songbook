c8de7b079afe851715c1f621151d36ee96332725
Marc Pete Casenas <dakillah@Siegharts-Mac.local>
2025-06-23 15:49:14 +0800

    and minor typo in script

--- script_v7_wip.js
+++ script_v7_wip.js
@@ -366,7 +366,7 @@ function toggleSelection(){
         selectionLabel2.textContent = "Title:";
         secondarySelectionDropdown.appendChild(selectSong);
         primarySelectionList.removeEventListener('click', handleSongChangeEvent);
-        primarySelectionList.palceholder = "Type in artist...";
+        primarySelectionList.placeholder = "Type in artist...";
         initArtistSelection();
 
     } else {
@@ -377,7 +377,7 @@ function toggleSelection(){
         selectionLabel2.textContent = "Artist:";
         secondarySelectionDropdown.appendChild(selectArtist);
         primarySelectionList.removeEventListener('click', handleArtistChangeEvent);
-        primarySelectionList.palceholder = "Type in song title...";
+        primarySelectionList.placeholder = "Type in song title...";
         initSongSelection();
     }
 
