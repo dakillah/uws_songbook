daf0dce7aa74ff2712c3e599b0a8d99b2d0dd08e
Marc Pete Casenas <dakillah@Siegharts-Mac.local>
2025-11-04 23:01:54 +0800

    Adding preview feature to newsong.html

--- script_newsong.js
+++ script_newsong.js
@@ -29,3 +29,25 @@ function saveData() {
     a.download = filename;
     a.click();
 }
+
+function openPreviewTab() {
+    var title = document.getElementById('title').value;
+    var artist = document.getElementById('artist').value;
+    var lyrics = document.getElementById('lyrics').value;
+
+    var checkboxes = document.querySelectorAll('.checkboxes input[type="checkbox"]');
+    var chords = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+
+    var data = {
+        title: title,
+        artist: artist,
+        lyrics: lyrics,
+        chords: chords
+    };
+
+    //var json = JSON.stringify(data);
+    localStorage.setItem('myData', JSON.stringify(data));
+
+    window.open("preview.html", "_blank");
+
+}
